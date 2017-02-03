4593fee081d05162a4d7177bec76610e6845a39d
taintech <rinattainov@gmail.com>
2017-02-03 23:34:34 +0100

    Added File reader actor and tests

---
+++ src/main/scala/com/taintech/actor/FileReader.scala
@@ -0,0 +1,35 @@
+package com.taintech.actor
+
+import akka.actor.{Actor, ActorLogging, Props}
+import com.taintech.actor.FileReader.{EOF, Next}
+
+import scala.io.Source
+
+/**
+  * Author: Rinat Tainov 
+  * Date: 03/02/2017
+  */
+class FileReader(filePath: String) extends Actor with ActorLogging {
+
+  var iterator: Iterator[String] = _
+
+  override def preStart(): Unit = iterator = Source.fromFile(filePath).getLines
+
+  def receive = {
+    case Next if iterator.nonEmpty => sender() ! iterator.next()
+    case Next if iterator.isEmpty => sender() ! EOF
+  }
+
+}
+
+object FileReader {
+
+  case object Next
+
+  case object EOF
+
+  def props(filePath: String): Props = {
+    require(filePath.nonEmpty)
+    Props(new FileReader(filePath))
+  }
+}
---
+++ src/test/scala/com/taintech/actor/FileReaderSpec.scala
@@ -0,0 +1,38 @@
+package com.taintech.actor
+
+import akka.actor.ActorSystem
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import com.taintech.actor.FileReader.{EOF, Next}
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, MustMatchers}
+
+/**
+  * Author: Rinat Tainov 
+  * Date: 03/02/2017
+  */
+class FileReaderSpec extends TestKit(ActorSystem("test-system"))
+  with FlatSpecLike
+  with ImplicitSender
+  with BeforeAndAfterAll
+  with MustMatchers {
+  override def afterAll {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  val testfile: String = this.getClass.getClassLoader.getResource("two-lines.txt").getPath
+
+  "Counter Actor" should "handle GetTopString message with using TestProbe" in {
+    val sender = TestProbe()
+    val reader = system.actorOf(FileReader.props(testfile))
+
+    sender.send(reader, Next)
+    val line1 = sender.expectMsgType[String]
+    line1 must equal("test line 1")
+
+    sender.send(reader, Next)
+    val line2 = sender.expectMsgType[String]
+    line2 must equal("test line 2")
+
+    sender.send(reader, Next)
+    sender.expectMsg(EOF)
+  }
+}
